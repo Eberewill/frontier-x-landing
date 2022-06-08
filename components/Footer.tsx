623f4902b8e34cc6ec94b77be7ac5205c459c3cb
Eberechi <eberewill@gmail.com>
2022-06-08 12:49:20 +0100

    work completed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="bg-x-black">
     <footer className="flex flex-wrap items-center justify-between p-3 m-auto">
-        <div className="container mx-auto flex flex-col flex-wrap items-center justify-between">
+        <div className="container mx-auto my-7 flex flex-col flex-wrap items-center justify-between">
             <Image
             src={Logog}
             width='200px'
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
                   />
                 </div>
               </div>
-              <div className="max-w-lg ml-10   w-full lg:max-w-xl sm:max-w-sm">
+              <div className="max-w-lg ml-10 lg:w-full rounded sm:max-w-sm">
                 <div className="relative">
                   <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                     <SearchIcon
@@ -36,7 +36,7 @@ const Navbar = () => {
                   <input
                     id="search"
                     name="search"
-                    className="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-gray-700 text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:border-white focus:ring-white focus:text-gray-900 sm:text-sm"
+                    className="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-lightBlack text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:border-white focus:ring-white focus:text-gray-900 sm:text-sm"
                     placeholder="Lorem ipsum"
                     type="search"
                   />
